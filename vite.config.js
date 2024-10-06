ee1ad3cb767e89d1a317894f1a06770c16a0efa3
Shaisab Mistry <shaisabmistry@gmail.com>
2024-10-06 19:17:38 -0400

    vercel

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://study-fetch-anthropic.vercel.app:3000',
         changeOrigin: true,
         secure: false,
       }
