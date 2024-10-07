517953c1481684b96c4031c43bbc73c8990dc1e3
Shaisab Mistry <shaisabmistry@gmail.com>
2024-10-07 02:08:09 -0400

    db change

--- api/db.js
+++ api/db.js
@@ -1,14 +1,18 @@
 import { MongoClient, ServerApiVersion} from 'mongodb';
+import dotenv from 'dotenv';
+dotenv.config()
 
 const mongoUri =process.env.VITE_MONGODB_URI;
-console.log(process.env.VITE_MONGODB_URI)
+const client = new MongoClient(mongoUri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
 
 export async function connectToDatabase() {
-  if (client && client.isConnected()) return client;
-  client = new MongoClient(mongoUri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
   await client.connect();
+  console.log(client.connect())
   return client;
 }
